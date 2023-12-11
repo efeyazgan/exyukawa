24e48826256557b0aa55838f257c8ee722ab6559
Efe <efe.yazgan@cern.ch>
2023-12-11 13:59:20 +0100

    1000 GeV

--- RandomizedParameters/produce_randomized_fragments.sh
+++ RandomizedParameters/produce_randomized_fragments.sh
@@ -7,6 +7,7 @@ python3 randomized_parameter_frag_create.py --mass 600 --proc cgbh
 python3 randomized_parameter_frag_create.py --mass 700 --proc cgbh
 python3 randomized_parameter_frag_create.py --mass 800 --proc cgbh
 python3 randomized_parameter_frag_create.py --mass 900 --proc cgbh
+python3 randomized_parameter_frag_create.py --mass 1000 --proc cgbh
 python3 randomized_parameter_frag_create.py --mass 200 --proc bgth
 python3 randomized_parameter_frag_create.py --mass 300 --proc bgth
 python3 randomized_parameter_frag_create.py --mass 350 --proc bgth
@@ -16,3 +17,4 @@ python3 randomized_parameter_frag_create.py --mass 600 --proc bgth
 python3 randomized_parameter_frag_create.py --mass 700 --proc bgth
 python3 randomized_parameter_frag_create.py --mass 800 --proc bgth
 python3 randomized_parameter_frag_create.py --mass 900 --proc bgth
+python3 randomized_parameter_frag_create.py --mass 1000 --proc bgth
